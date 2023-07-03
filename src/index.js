98126c0ba70f8aa70654e5cdfad7689d8d840678
Alexander <alexkorpol1964@gmail.com>
2023-07-03 15:51:42 +0300

    Repair old reduction index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from "react-redux";
-import { persistor, store } from 'redux/store';
-import { PersistGate } from 'redux-persist/lib/integration/react';
+
+import { store } from "./redux/store";
 import  App  from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <PersistGate persistor={persistor} loading={null}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </PersistGate>
+    <Provider store={store}>
+      < App />
+    </Provider>
 
   </React.StrictMode>
 );
